3454a19399befa31120e09e74281b96962505d26
Tomasz Rostanski <rozteck@interia.pl>
2012-07-11 12:05:41 +0200

    Camera: Check for jpeg quality of zero value.
    This is port of:
    https://www.codeaurora.org/gitweb/quic/la/?p=platform/hardware/qcom/camera.git;a=commit;h=ffa543c9e75a5489cde2dda9daeb3e46aba8ccce

--- QualcommCameraHardware.cpp
+++ QualcommCameraHardware.cpp
@@ -2409,6 +2409,11 @@ bool QualcommCameraHardware::native_jpeg_encode(void)
     LOGV("%s E", __FUNCTION__);
     int jpeg_quality = mParameters.getInt("jpeg-quality");
     if (jpeg_quality >= 0) {
+        //Application can pass quality of zero
+        //when there is no back sensor connected.
+        //as jpeg quality of zero is not accepted at
+        //camera stack, pass default value.
+        if(jpeg_quality == 0) jpeg_quality = 85;
         LOGV("native_jpeg_encode, current jpeg main img quality =%d",
              jpeg_quality);
         if(!LINK_jpeg_encoder_setMainImageQuality(jpeg_quality)) {
@@ -2419,6 +2424,11 @@ bool QualcommCameraHardware::native_jpeg_encode(void)
 
     int thumbnail_quality = mParameters.getInt("jpeg-thumbnail-quality");
     if (thumbnail_quality >= 0) {
+        //Application can pass quality of zero
+        //when there is no back sensor connected.
+        //as quality of zero is not accepted at
+        //camera stack, pass default value.
+        if(thumbnail_quality == 0) thumbnail_quality = 85;
         LOGV("native_jpeg_encode, current jpeg thumbnail quality =%d",
              thumbnail_quality);
         if(!LINK_jpeg_encoder_setThumbnailQuality(thumbnail_quality)) {
